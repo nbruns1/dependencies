4b6509c7c1218ecfd7826fb4b20f8f27a9555e05
U-Niklas-HP\Niklas <nbruns@uni-bremen.de>
2016-11-23 14:27:17 +0100

    exclude minisat as boolector dep for cygwin build

--- boolector-2.2.0/setup.sh
+++ boolector-2.2.0/setup.sh
@@ -2,7 +2,7 @@
 
 version=2.2.0
 
-dependencies="lingeling-ayv-86bf266-140429 minisat-git"
+dependencies="lingeling-ayv-86bf266-140429"
 
 patches_dir=$base_dir/boolector-$version
 cmake_files_dir=$base_dir/boolector-$version
