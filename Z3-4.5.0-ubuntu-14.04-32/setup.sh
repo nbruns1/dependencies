797f93a6081a641b50333362d95f3ed794b48d68
Niklas Bruns <nbruns@uni-bremen.de>
2017-06-16 17:06:23 +0200

    fix

--- Z3-4.5.0-ubuntu-14.04-32/setup.sh
+++ Z3-4.5.0-ubuntu-14.04-32/setup.sh
@@ -5,4 +5,4 @@ branch=z3-4.5.0
 
 cmake_config_file="$base_dir/Z3-4.5.0-ubuntu-14.04-64/Z3Config.cmake"
 
-source $base_dir/Z3-4.5.0-ubuntu-14.04-64/shared.sh
+source $base_dir/Z3-4.5.0-ubuntu-14.04-32/shared.sh
